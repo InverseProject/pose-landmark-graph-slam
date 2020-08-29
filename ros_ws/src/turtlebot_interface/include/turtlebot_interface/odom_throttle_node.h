7e92c85afcaf8f8ca622d8c145666aca5f9c5dc5
Deepak Talwar <deepaktalwardt@gmail.com>
2020-08-29 07:06:38 -0700

    added header file for the node

---
+++ ros_ws/src/turtlebot_interface/include/turtlebot_interface/odom_throttle_node.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <iostream>
+#include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
+
+namespace turtlebot_interface
+{
+
+class NodeOdometryThrottle
+{
+public:
+    NodeOdometryThrottle(
+        const std::string& in_odom_topic,
+        const std::string& out_odom_topic,
+        float distance_threshold,
+        float rotation_threshold);
+    
+    ~NodeOdometryThrottle();
+
+    void Callback(const nav_msgs::OdometryConstPtr& odom_msg);
+
+private:
+
+    // Input parameters
+    float distance_threshold = 0.05; // in meters
+    float rotation_threshold = 0.1; // in radians (~ 5 deg)
+
+    // Current variables
+    nav_msgs::Odometry prev_odom_msg_published_;
+    
+};
+
+}  // namespace turtlebot_interface
