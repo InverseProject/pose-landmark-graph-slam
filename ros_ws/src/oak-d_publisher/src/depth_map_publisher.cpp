b1fa8605e34e29887cded36fa20a5f9edb197f82
saching13 <sachin.guruswamy@gmail.com>
2020-09-01 22:34:27 -0700

    updated cmake

--- ros_ws/src/oak-d_publisher/src/depth_map_publisher.cpp
+++ ros_ws/src/oak-d_publisher/src/depth_map_publisher.cpp
@@ -52,7 +52,7 @@ DepthMapPublisher::publisher(){
     // vector<uint16_t> img_data(720*1280, 0);    
     // unsigned char* depth_data = reinterpret_cast<unsigned char*>(img_data.data());
     
-    cv::Mat img_data = cv::Mat(rows, cols, CV_16U1)
+    cv::Mat img_data = cv::Mat(720,1280 , CV_16U)
     unsigned char* depth_data = reinterpret_cast<unsigned char*>(m.data);
     // m.data
     while(ros::ok()){
